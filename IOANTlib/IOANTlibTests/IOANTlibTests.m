92b2db490aade888c370a27746aa63a8966b84bf
lsgit <848949609@qq.com day06 lsgit>
2014-09-30 21:37:37 +0800

    取汉字首字母，中文语句转拼音 IOANTChineseToPinYin IOANTpinyin

--- IOANTlib/IOANTlibTests/IOANTlibTests.m
+++ IOANTlib/IOANTlibTests/IOANTlibTests.m
@@ -5,30 +5,3 @@
 //  Created by liushuai on 14-9-30.
 //  Copyright (c) 2014年 Liushuai. All rights reserved.
 //
-
-#import <XCTest/XCTest.h>
-
-@interface IOANTlibTests : XCTestCase
-
-@end
-
-@implementation IOANTlibTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
