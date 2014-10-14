d7c3aefdb3f93ad666662a570acb2d1f098f9f94
lsgit <848949609@qq.com day06 lsgit>
2014-10-14 21:40:49 +0800

    自定义toast

--- IOANTlib/IOANTlib/common/pinyin/IOANTChineseToPinyin.h
+++ IOANTlib/IOANTlib/common/pinyin/IOANTChineseToPinyin.h
@@ -4,11 +4,11 @@
 @interface IOANTChineseToPinyin : NSObject {
 
 }
-//将中文语句转化为拼音 每个汉字字母全部大写
-//中国 ZHONGGUO
+/** 将中文语句转化为拼音 每个汉字字母全部大写
+中国 ZHONGGUO */
 + (NSString *) pinyinFromChiniseStringAllUpper:(NSString *)string;
-//将中文语句转化为拼音 每个汉字首字母大写
-//中国 ZhongGuo
+/** 将中文语句转化为拼音 每个汉字首字母大写
+中国 ZhongGuo */
 + (NSString *) pinyinFromChiniseStringCapitalUpper:(NSString *)string;
 + (char) sortSectionTitle:(NSString *)string; 
 @end
